f42f7a0496ebbca2744bfe0abb69330a929e48fe
AglBot <146909834+AglBot@users.noreply.github.com>
2023-10-07 13:17:29 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/TeamHMT/Auto-search-tamil-bot.git /Auto-search-tamil-bot
+  git clone https://github.com/AglBot/Auto-search /Auto-search
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Auto-search-tamil-bot
+  git clone $UPSTREAM_REPO /Auto-search
 fi
-cd /Auto-search-tamil-bot
+cd /Auto-search
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
